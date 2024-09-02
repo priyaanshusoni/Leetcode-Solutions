64bd6eaf8a3ad8c80f3b69e00f4aa97a4274ac44
Priyanshu Soni <98266423+priyaanshusoni@users.noreply.github.com>
2024-09-02 18:31:40 +0530

    Time: 126 ms (5.64%), Space: 26.2 MB (98.81%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,52 +1,26 @@
 class Solution {
 public:
 
-int func(vector<int>&nums , int index , int element){
-    for(int i=0;i<index;i++){
-        if(nums[i]>element){
-            return nums[i];
-        }
-
-    }
 
 
-    return -1;
-}
+  
+
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int>ans(nums.size());
+        vector<int>ans(nums.size(),-1);
+        int n= nums.size();
 
-
-      for(int i=0;i<nums.size();i++){
-        int isFound = false;
-        int index=-1;
-        for(int j=i+1;j<nums.size();j++){
-            if(nums[j]>nums[i]){
-                isFound = true;
-                 ans[i]=nums[j];
-                break;
-            }
-        }
-
-        if(!isFound){
-            int ele = func(nums,i,nums[i]);
-
-            if(ele>=0 || nums[i]!=-1){
-                ans[i]=ele;
-            }
-
-            else{
-                ans[i]=-1;
-            }
-        }
-
-
-
-
+      for(int i=0;i<n;i++){
       
-        
+              for(int j=i+1;j<=i+(n)-1;j++){
+                int index = j%n;
+
+                if(nums[index]>nums[i]){
+                    ans[i]=nums[index];
+                    break;
+                }
+              }
       }
-
-
+              
         return ans;
     }
 };
