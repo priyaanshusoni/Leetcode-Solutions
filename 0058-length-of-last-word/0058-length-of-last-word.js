80a3f9c37c6f9751bc4b354412b21ac702cda762
Priyanshu Soni <98266423+priyaanshusoni@users.noreply.github.com>
2024-10-26 10:07:45 +0530

    Time: 0 ms (100%), Space: 49.1 MB (32.31%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    const arr= s.trim().split(' ');
+
+    const ans = arr[arr.length-1];
+
+    return ans.length;
+
+
+
+};
