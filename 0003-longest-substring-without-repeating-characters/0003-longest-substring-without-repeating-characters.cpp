ea027ff20584803146fa5a4fe171cb95e5c229c5
Priyanshu Soni <98266423+priyaanshusoni@users.noreply.github.com>
2024-11-16 15:20:49 +0530

    Time: 855 ms (5.02%), Space: 280.7 MB (5.01%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,67 +1,29 @@
+#include<algorithm>
+
 class Solution {
 public:
-
-
-//this function cheks for whether a substring inside a string is unique or not ! 
-//    bool isUniqueSubstr(string s){
-
-//      bool isUnique = true;
-//      for(int i=0;i<s.length();i++){
-
-//         for(int j=i+1;j<s.length();j++){
-//               if(s[j]==s[i]){
-//                 isUnique = false;
-//                 break;
-//               }
-//         }
-//      }
-
-//      if(isUnique) return true;
-
-
-//      return false;
-
-
-//    }
     int lengthOfLongestSubstring(string s) {
-        
-       unordered_map<char , int>mpp;
-        int maxCnt=0;
+        int longest = 1;
+       if(s.length()==0) return 0;
+        map<char , int>mpp;
 
+        for(int i=0;i<s.length();i++){
+            string ans = "";
+            for(int j=i;j<s.length();j++){
+                if(mpp.find(s[j])!=mpp.end()){
+                  break;
+                }
+                ans+=s[j];
+                mpp[s[j]]=1;
+                
 
-          for(int i=0;i<s.length();i++){
-           
-             int cnt=0;
-           
-            for(int j=i;j<s.length();j++){ 
-                if(mpp.empty() || mpp.find(s[j])==mpp.end()){
-                     mpp[s[j]] =1;
-                       cnt++;
-                     maxCnt = max(maxCnt , cnt);
-                }
-                else{
-                   
-                    mpp.clear();
-                    break;
-                }
             }
+            longest = max(longest , (int)ans.length());
+            mpp.clear();
+            // longest=max(cnt,longest);
+          
+        }
 
-
-
-          }
-
-
-
-
-
-
-        
-
-
-
-        return maxCnt;
-
-
-
+        return longest;
     }
 };
