df758e05644aa2c580e16ecef0fea157c282bc29
Priyanshu Soni <98266423+priyaanshusoni@users.noreply.github.com>
2024-11-25 19:06:48 +0530

    Time: 4 ms (12.41%), Space: 14.1 MB (25.02%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+
+void AllSubSequences(vector<int>&arr, vector<vector<int>>&ans , vector<int>temp , int index , int n){
+
+if(index==n){
+    ans.push_back(temp);
+    return;
+}
+
+AllSubSequences(arr , ans , temp , index+1 , n);
+temp.push_back(arr[index]);
+AllSubSequences(arr , ans , temp , index+1 , n);
+
+
+
+
+
+}
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>>ans;
+        vector<int>temp;
+
+         AllSubSequences(nums,ans , temp , 0 , n);
+
+
+         return ans;
+ 
+    }
+};
