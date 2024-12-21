59f7528f0708ff8508018f64396da1ba306166c8
Priyanshu Soni <98266423+priyaanshusoni@users.noreply.github.com>
2024-12-21 22:56:06 +0530

    Time: 7 ms (5.4%), Space: 16.7 MB (5.66%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
 
-void AllSubsequences(vector<vector<int>>&ans , vector<int>temp , vector<int>nums , int n , int index){
-if(index>=n){
+void Allsubsequences(vector<vector<int>>&ans , vector<int>temp , vector<int>nums , int n , int index){
+
+   if(index==n){
     ans.push_back(temp);
     return;
+   }
+
+    Allsubsequences(ans , temp , nums , n , index+1); //--->  base case
+    temp.push_back(nums[index]);
+
+     Allsubsequences(ans , temp , nums , n , index+1);
+
 }
 
-AllSubsequences(ans , temp , nums, n , index+1);
-temp.push_back(nums[index]);
-AllSubsequences(ans , temp , nums, n , index+1);
-
-
-}
-
+// [1 ,2 , 3 ] 
+// N = > total no of subsets = 2^N 
     vector<vector<int>> subsets(vector<int>& nums) {
-     vector<vector<int>>ans;
-     int n = nums.size();
+      vector<vector<int>>ans;
+      vector<int>temp;
 
-     vector<int>temp;
+      int n = nums.size();
 
-     AllSubsequences(ans , temp ,nums, n , 0);
+      Allsubsequences(ans , temp , nums , n ,0 );
 
-     return ans;
+
+      return ans;
     }
 };
