213d85e6d24fe4b5cab854d52194924a53c92a4c
Priyanshu Soni <98266423+priyaanshusoni@users.noreply.github.com>
2025-02-09 20:18:28 +0530

    Time: 60 ms (77.58%), Space: 50.1 MB (68.62%) - LeetHub

---
+++ 2741-function-composition/2741-function-composition.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    // Her functions is an array And I need to return the array of functions 
+   return (input)=>{
+
+    
+     return functions.reduceRight((acc,curr)=>{
+        // curr is second last element 
+        // acc is last element of the functions array
+        
+        return curr(acc)
+     },input)
+
+
+   }
+    }
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
