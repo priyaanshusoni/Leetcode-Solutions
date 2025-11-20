f2e885ac7ff843624fc6828f3c75640ba4c87779
Priyanshu Soni <98266423+priyaanshusoni@users.noreply.github.com>
2025-11-20 15:46:07 +0530

    Time: 14 ms (21.49%), Space: 7.7 MB (87.01%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0){
-            return 0;
-        }
+        //5
 
-        if(n==1){
-            return 1;
-        }
+
+
+        if (n==0 || n==1 ) return n;
 
 
         return fib(n-1) + fib(n-2);
+        
     }
 };
