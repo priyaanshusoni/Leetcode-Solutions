0d83d5980c7dd79e7a34de4d0c23dd559c32ec79
Priyanshu Soni <98266423+priyaanshusoni@users.noreply.github.com>
2026-04-07 00:56:30 +0530

    Time: 63 ms (9.83%), Space: 36.8 MB (5.37%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,44 +2,32 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
 
-        int dup = -1;
-        int missing = -1;
+       map<int , int>mpp;
+       int dup = -1;
+       int missing = -1;
+
+       for (auto it : nums) {
+        mpp[it]+=1;
+       }
 
 
-
-        for(int i = 1 ; i<=nums.size();i++){
-               int cnt = 0;
-
-            for(int j = 0 ; j<nums.size();j++) {
-
-            
-
-                if(nums[j]==i) {
-
-              
-                    cnt++;
-
-
-                              if(cnt == 2) {
-                    dup = nums[j];
-                }
-                }
-
-
-            }
-
-
-            if(cnt == 0) {
-                missing = i;
-            }
-
+       for(int i=1 ;i<=nums.size();i++){
+        if(mpp[i]==2) {
+            dup = i;
         }
+        else if (!mpp[i]) {
+            missing = i;
+        }
+       }
 
 
 
 
-        return { dup , missing};
+       
 
+
+
+       return {dup , missing};
 
 
 
