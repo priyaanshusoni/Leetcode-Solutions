eaf253aa92ecad97224319ee11dcf60e264b53e3
Priyanshu Soni <98266423+priyaanshusoni@users.noreply.github.com>
2026-04-20 22:09:19 +0530

    Time: 351 ms (5.16%), Space: 19.3 MB (27.03%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,20 +1,12 @@
 class Solution:
     def fib(self, n: int) -> int:
-        ans = []
-        
-        ans.append(0)
-        ans.append(1)
-        ans.append(1)
+      
+      if n==1 or n==0:
+        return n
 
-        first = ans[1]
-        second = ans[2]
-        for i in range(2,n):
-            third = first + second
-            ans.append(third)
-            first = second
-            second = third
-        print(ans)
-        return ans[n]
-   
+      return self.fib(n-1) + self.fib(n-2)
+     
+    
+     
         
         
