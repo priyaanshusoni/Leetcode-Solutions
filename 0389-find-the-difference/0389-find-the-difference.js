63cf8f2fa2bb110958b8ed191686ae1e572965cc
Priyanshu Soni <98266423+priyaanshusoni@users.noreply.github.com>
2026-07-05 00:44:28 +0530

    Time: 11 ms (8.58%), Space: 56.1 MB (50.89%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -15,24 +15,31 @@ var findTheDifference = function (s, t) {
             map.set(t[i], (map.get(t[i]) || 0) + 1)
         }
 
-       else  map.set(t[i], 1)
+        else {
+            map.set(t[i], 1)
+        }
+
+
+
+
+     
+
+
     }
 
 
 
-
-
-
-
-    for (let i = 0; i < s.length; i++) {
-        if (map.has(s[i])) {
-            map.set(s[i], (map.get(s[i])) - 1)
+    for (const value of s) {
+           if (map.has(value)) {
+            map.set(value, (map.get(value) - 1))
         }
     }
 
 
+
+
     for (const [key, value] of map) {
-        console.log(`Key ${key} Value : ${value}`)
+       
         if (value === 1) return String(key)
     }
 
