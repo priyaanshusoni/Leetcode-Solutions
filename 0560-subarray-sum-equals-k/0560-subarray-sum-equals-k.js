e8955fb8fe3b51fc9bb02769e4be72cebed8c3a0
Priyanshu Soni <98266423+priyaanshusoni@users.noreply.github.com>
2026-08-11 22:58:43 +0530

    Time: 1565 ms (11.56%), Space: 57.8 MB (88.74%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+     let number = 0
+
+    for(let i=0 ; i<nums.length ; i++){
+        let sum = 0;
+         for (let j = i ; j<nums.length; j++){
+
+            sum +=nums[j];
+            if(sum=== k) number++
+             
+         }
+    }
+
+
+    return number;
+    
+};
