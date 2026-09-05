357769751491a1dcf49fce98e77995ef024cb6d6
Priyanshu Soni <98266423+priyaanshusoni@users.noreply.github.com>
2026-09-06 02:44:34 +0530

    Time: 34 ms (73.1%), Space: 73 MB (32.67%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -4,33 +4,56 @@
  */
 var threeSum = function(nums) {
 
-    let ans = [];
 
     nums.sort((a,b)=>a-b)
 
   
+    let ans = []
+
+
+ 
+
 
     for(let i=0 ; i<nums.length ; i++){
-        if(i > 0 &&  nums[i]===nums[i-1]) continue;
-          let set = new Set()
-        for(let j=i+1; j<nums.length ; j++){
 
-            let first = nums[i];
-            let second = nums[j];
+        if(i>0 && nums[i]===nums[i-1]) continue;
 
-            let third = -(first + second)
+        let j = i+1;
+        let k = nums.length-1;
 
-            if(set.has(third)){
-                ans.push([first , second , third ])
-                while(nums[j+1]===nums[j]) j++;
+        while( j< k){
+
+
+
+            let sum = nums[i] + nums[j] + nums[k];
+
+            if(sum<0){
+                //negative
+                j++;
+                
+
             }
+            else if(sum>0){
+                k--
 
-            set.add(second);
+            }
+            else {
+                ans.push([nums[i] , nums[j] , nums[k]])
+                j++;
+                while(nums[j]===nums[j-1]){
+                    j++;
 
+                }
+             
+
+            }
         }
+
     }
 
 
     return ans;
+
+  
     
 };
