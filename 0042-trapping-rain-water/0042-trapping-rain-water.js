22be15afeec433135ee0903a46da81e8c957e9cb
Priyanshu Soni <98266423+priyaanshusoni@users.noreply.github.com>
2026-09-15 00:35:19 +0530

    Time: 22 ms (13.21%), Space: 61.8 MB (9.37%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -0,0 +1,55 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+
+
+
+    let ans = 0;
+
+
+    let prefix = [];
+
+    let suffix = [];
+
+    let prefixMax = height[0];
+    prefix[0] = prefixMax;
+    let suffixMax = height[height.length - 1];
+    suffix[height.length-1] = suffixMax
+
+
+
+    for (let i = 1; i < height.length; i++) {
+
+        prefixMax = Math.max(prefixMax, height[i])
+        prefix.push(prefixMax);
+
+    }
+
+
+    for (let i = height.length - 2; i >= 0; i--) {
+
+        suffixMax = Math.max(suffixMax, height[i])
+        suffix[i] = suffixMax;
+
+    }
+
+
+
+
+
+    for (let i = 0; i < height.length; i++) {
+
+        let maxLeft = prefix[i];
+        let maxRight = suffix[i]
+
+        let trappedWater = Math.min(maxLeft, maxRight) - height[i];
+
+        ans += trappedWater
+    }
+
+
+    return ans;
+
+};
